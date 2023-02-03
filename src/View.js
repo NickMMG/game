797087cd6c8a12e0a7b5e9c6dd927bee9fa350b6
Nikita <nikita21070@gmail.com>
2023-02-03 15:05:59 +0300

    Realese1

--- src/View.js
+++ src/View.js
@@ -7,7 +7,7 @@ class View {
     const yourTeamName = 'Elbrus';
 
     // Тут всё рисуем.
-    // console.clear();
+    console.clear();
     console.log(track.join(''));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
